416f7461e9b0f0c050bbf11cda6f2f52e058f1a7
Cyril Gadal <cyril.gadal@gmail.com>
2023-06-06 12:34:56 +0200

    Changing repo name

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name="2LshallowPy",
+      packages=find_packages(),
+      python_requires='>=3',
+      install_requires=[
+        "numpy", "matplotlib"],
+      author='Cyril Gadal',
+      license='Apache-2.0',
+      version='0.0.1',
+      zip_safe=False,
+      )
